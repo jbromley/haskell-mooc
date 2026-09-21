dedc30954194526ab730047688924ae9b90a6d0a
J. Bromley <jay@appliedinvention.com>
2026-09-21 12:08:49 -0700

    Work on set 3b
    Worked on problems 1 - 3.

--- exercises/Set3b.hs
+++ exercises/Set3b.hs
@@ -20,7 +20,6 @@
 -- Feel free to use if-then-else, guards, and ordering functions (< and > etc.).
 --
 -- The tests will check that you haven't added imports :)
-
 {-# LANGUAGE NoImplicitPrelude #-}
 
 module Set3b where
@@ -37,9 +36,9 @@ import Mooc.Todo
 -- Examples:
 --   buildList 1 5 2 ==> [1,1,1,1,1,2]
 --   buildList 7 0 3 ==> [3]
-
 buildList :: Int -> Int -> Int -> [Int]
-buildList start count end = todo
+buildList _ 0 end = [end]
+buildList s c end = s : buildList s (c - 1) end
 
 ------------------------------------------------------------------------------
 -- Ex 2: given i, build the list of sums [1, 1+2, 1+2+3, .., 1+2+..+i]
@@ -47,9 +46,12 @@ buildList start count end = todo
 -- Use recursion and the : operator to build the list.
 --
 -- Ps. you'll probably need a recursive helper function
-
 sums :: Int -> [Int]
-sums i = todo
+sums i = sums' 1 i
+  where
+    sums' n end
+      | n == end + 1 = []
+      | otherwise = (n * (n + 1)) `div` 2 : sums' (n + 1) end
 
 ------------------------------------------------------------------------------
 -- Ex 3: define a function mylast that returns the last value of the
@@ -61,9 +63,10 @@ sums i = todo
 -- Examples:
 --   mylast 0 [] ==> 0
 --   mylast 0 [1,2,3] ==> 3
-
 mylast :: a -> [a] -> a
-mylast def xs = todo
+mylast def [] = def
+mylast def (x:[]) = x
+mylast def (x:xs) = mylast def xs
 
 ------------------------------------------------------------------------------
 -- Ex 4: safe list indexing. Define a function indexDefault so that
@@ -79,7 +82,6 @@ mylast def xs = todo
 --   indexDefault [10,20,30] 2 7         ==>  30
 --   indexDefault [10,20,30] 3 7         ==>  7
 --   indexDefault ["a","b","c"] (-1) "d" ==> "d"
-
 indexDefault :: [a] -> Int -> a -> a
 indexDefault xs i def = todo
 
@@ -95,7 +97,6 @@ indexDefault xs i def = todo
 --   sorted [2,7,7] ==> True
 --   sorted [1,3,2] ==> False
 --   sorted [7,2,7] ==> False
-
 sorted :: [Int] -> Bool
 sorted xs = todo
 
@@ -107,7 +108,6 @@ sorted xs = todo
 --   sumsOf []       ==>  []
 --
 -- Use pattern matching and recursion (and the list constructors : and [])
-
 sumsOf :: [Int] -> [Int]
 sumsOf xs = todo
 
@@ -122,7 +122,6 @@ sumsOf xs = todo
 --   merge [1,1,6] [1,2]   ==> [1,1,1,2,6]
 --   merge [1,2,3,20] [7]  ==> [1,2,3,7,20]
 --   merge [1] [2,3,4,5,6] ==> [1,2,3,4,5,6]
-
 merge :: [Int] -> [Int] -> [Int]
 merge xs ys = todo
 
@@ -146,7 +145,6 @@ merge xs ys = todo
 --   mymaximum (<) 4 [1,3,2] ==> 1    -- note changed biggerThan
 --   mymaximum (\(a,b) (c,d) -> b > d) ("",0) [("Banana",7),("Mouse",8)]
 --     ==> ("Mouse",8)
-
 mymaximum :: (a -> a -> Bool) -> a -> [a] -> a
 mymaximum bigger initial xs = todo
 
@@ -160,7 +158,6 @@ mymaximum bigger initial xs = todo
 -- of the longer list.
 --
 -- Use recursion and pattern matching. Do not use any library functions.
-
 map2 :: (a -> b -> c) -> [a] -> [b] -> [c]
 map2 f as bs = todo
 
@@ -184,6 +181,5 @@ map2 f as bs = todo
 --
 -- maybeMap (\x -> Nothing) [1,2,3]
 --   ==> []
-
 maybeMap :: (a -> Maybe b) -> [a] -> [b]
 maybeMap f xs = todo
